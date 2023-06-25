b7bf98f6ec11d5ecfd2ff78fe33c4fe471b65251
Kumina <redgoat98@gmail.com>
2023-06-25 16:59:55 +0300

    Finished the script

--- kumiscripts.py
+++ kumiscripts.py
@@ -1,65 +1,76 @@
 import os
-import subprocess
 import requests
+import subprocess
 from tqdm import tqdm
 
-def run_script(script_path):
-    try:
-        subprocess.check_output(['python', script_path])
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running {script_path}: {e.output.decode()}")
+def install_script(script_name, script_directory, script_url):
+    script_path = os.path.join(script_directory, script_name)
 
-def install_script(script_url, script_path):
-    response = requests.get(script_url)
+    try:
+        response = requests.get(script_url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"Error occurred while downloading script '{script_name}': {e}")
+        return
+
     with open(script_path, 'wb') as file:
         file.write(response.content)
+    print(f"Script '{script_name}' installed successfully.")
 
 def check_and_install_scripts(script_directory, website_url):
-    response = requests.get(website_url)
-    required_scripts = response.json()
+    scripts_url = f"{website_url}/scripts.json"
+    try:
+        response = requests.get(scripts_url)
+        response.raise_for_status()
+        script_data = response.json()
+    except requests.exceptions.HTTPError as e:
+        print(f"Error occurred while retrieving script information: {e}")
+        return
+    except ValueError as e:
+        print(f"Error occurred while parsing script data: {e}")
+        return
 
-    missing_scripts = []
-    for script in required_scripts:
-        script_path = os.path.join(script_directory, script['filename'])
+    scripts_to_install = script_data.get('scripts', [])
+    if not scripts_to_install:
+        print("No scripts found to install.")
+        return
+
+    print("Installing missing scripts:")
+    for script in tqdm(scripts_to_install):
+        script_name = script['name']
+        script_url = script['url']
+        script_path = os.path.join(script_directory, script_name)
         if not os.path.isfile(script_path):
-            missing_scripts.append(script)
-    
-    if missing_scripts:
-        print("Installing missing scripts:")
-        for script in tqdm(missing_scripts):
-            install_script(script['url'], os.path.join(script_directory, script['filename']))
-        print("Installation completed.")
+            install_script(script_name, script_directory, script_url)
 
-def prompt_user(script_directory):
-    script_files = [filename for filename in os.listdir(script_directory) if filename.endswith('.py')]
+    print("Installation completed.")
+
+def run_script(scripts_directory):
+    script_files = os.listdir(scripts_directory)
     if not script_files:
-        print("No script files found in the directory.")
+        print("No scripts found in the directory.")
         return
     
     print("Available scripts:")
-    for index, script_file in enumerate(script_files, start=1):
-        print(f"{index}. {script_file}")
-    
-    script_choice = input("Enter the number corresponding to the script you want to run: ")
+    for i, script_file in enumerate(script_files):
+        print(f"{i + 1}. {script_file}")
 
-    try:
-        script_index = int(script_choice) - 1
-        if 0 <= script_index < len(script_files):
-            script_path = os.path.join(script_directory, script_files[script_index])
-            run_script(script_path)
-        else:
-            print("Invalid script choice.")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
+    while True:
+        try:
+            script_choice = int(input("Enter the number of the script you want to run (or 0 to exit): "))
+            if script_choice == 0:
+                return
+            script_index = script_choice - 1
+            selected_script = script_files[script_index]
+            script_path = os.path.join(scripts_directory, selected_script)
+            subprocess.run(["python", script_path])
+        except ValueError:
+            print("Invalid input. Please enter a valid script number.")
+        except IndexError:
+            print("Invalid script number. Please try again.")
 
-def check_scripts_folder(script_directory):
-    if not os.path.exists(script_directory):
-        os.makedirs(script_directory)
-        print(f"The scripts folder '{script_directory}' was created.")
-
-scripts_directory = 'scripts'
+scripts_directory = './scripts'
 website_url = 'https://kumina.wtf/scripts'
 
-check_scripts_folder(scripts_directory)
 check_and_install_scripts(scripts_directory, website_url)
-prompt_user(scripts_directory)
+run_script(scripts_directory)
